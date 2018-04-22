15e16808660f4ee00be1453273e34b9b63c30bae
cjqw <2462457751@qq.com>
2018-04-22 23:21:33 +0800

    train the model

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ config = {"data-path":"./data/lfw/",
           "batchs":100,
           "train-datasets":300,
           "model-path":"./model/",
-          "img-shape":(3,64,64),
+          "img-shape":(64,64,3),
           "feature-shape":(40,),
+          "feature-number":40,
           "train":True}
 
 def get_config(para):
@@ -13,3 +14,15 @@ def get_config(para):
     except:
         value = None
     return value
+
+# tensorflow configuration
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+import os
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+set_session(sess)
--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 from fuel.datasets import CelebA
 from config import get_config
 from model import build_generator,build_discriminator
+from keras.models import Model
+from keras.layers import Input
 import keras
 import cv2
 
@@ -17,6 +19,26 @@ def convert_rgb_img(img):
     x[:,:,2] = img[0,:,:]
     return x
 
+def gen_noise(n,shape):
+    return np.random.randint(2,size=(n,shape))
+
+def get_batch(train_set,handle,l):
+    r = l + get_config("batchs")
+    imgs,features = train_set.get_data(handle,slice(l,r))
+    imgs = imgs / 255
+    imgs = np.moveaxis(imgs,1,3)
+    return imgs,features
+
+def get_dis_train_pair(imgs,features):
+    """Get train pair for discriminator."""
+    batch = get_config("batchs")
+    x,y = imgs,features
+    return x,y
+
+def get_gen_train_pair():
+    x = gen_noise(get_config("batchs"),get_config("feature-number"))
+    return x,x
+
 def train_model():
     train_set = CelebA('64',which_sets=('train',))
     print(train_set.num_examples)
@@ -25,22 +47,35 @@ def train_model():
     n_batchs = (get_config("train-datasets") or train_set.num_examples) // get_config("batchs")
     generator = build_generator()
     discriminator = build_discriminator()
+    discriminator.compile(loss='categorical_crossentropy', optimizer='RMSprop')
+    generator.compile(loss='binary_crossentropy', optimizer='RMSprop')
+
+
+    gan_input = Input(shape=get_config("feature-shape"))
+    H = generator(gan_input)
+    gan_output = discriminator(H)
+    gan = Model(gan_input,gan_output)
+    gan.compile(loss='categorical_crossentropy', optimizer='RMSprop')
 
     for i in range(get_config("epochs")):
         for j in range(n_batchs):
-            l = j * get_config("batchs")
-            r = (j + 1) * get_config("batchs")
-            imgs,features = train_set.get_data(handle,slice(l,r))
-            imgs = imgs / 255
-            imgs = np.moveaxis(imgs,1,3)
+            imgs,features = get_batch(train_set,handle,j*get_config("batchs"))
             # x = imgs[0]
             # for ii in range(64):
             #     for jj in range(64):
             #         x[ii][jj] = x[ii][jj][::-1]
             # cv2.imshow("img",x)
             # cv2.waitKey(0)
-            print(l,r)
-            # train models
+
+            # train Discriminator
+            discriminator.trainable = True
+            X,Y = get_dis_train_pair(imgs,features)
+            d_loss = discriminator.train_on_batch(X,Y)
+
+            # train Generator
+            discriminator.trainable = False
+            X,Y = get_gen_train_pair()
+            g_loss = gan.train_on_batch(X,Y)
 
     # save models
 
